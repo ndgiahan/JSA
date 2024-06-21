39af46222d683288193419be7a4e46715f7e1301
ndgiahan <134265323+ndgiahan@users.noreply.github.com>
2024-06-21 20:35:25 +0700

    Update main.js

--- Day5/main.js
+++ Day5/main.js
@@ -1,10 +1,12 @@
-
+//Bước 1: Lấy giá trị trong từng ô
 var nameInp = document.getElementById("userNameInp");
 var passInp = document.getElementById("passInp");
 
  function InRaManHinh() {
+  //Lưu vào local storage bằng cú pháp setItem. TRong đó'TenDN' gọi là key; 'nameInp.value' là biến đã lấy giá trị ở bước 1
     localStorage.setItem('TenDN', nameInp.value);
     localStorage.setItem('MatKhau', passInp.value);
+  //Lấy dữ liệu từ local storage in ra màn hình
     document.getElementById("userNameOutp").innerHTML = localStorage.getItem('TenDN');
     document.getElementById("passOutp").innerHTML = localStorage.getItem('MatKhau')
  }
